d3b9f232b721baa3c225705254e546048352416a
apricot57 <kssr.1990@gmail.com>
2026-05-25 08:55:30 +0530

    feat: configure fully immersive edge-to-edge system navigation and safe areas

--- android/app/src/main/java/com/vibechat/app/MainActivity.java
+++ android/app/src/main/java/com/vibechat/app/MainActivity.java
@@ -1,5 +1,25 @@
 package com.vibechat.app;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Enable edge-to-edge layout so app flows behind status and navigation/gesture bars
+        Window window = getWindow();
+        WindowCompat.setDecorFitsSystemWindows(window, false);
+        
+        // Make system bars completely transparent
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.TRANSPARENT);
+        }
+    }
+}
